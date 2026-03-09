9fb7c7277f9c7db67ad99a6d5b5ed13d150c4afe
Starlord427 <alanarmandomontiel@gmail.com>
2026-03-09 10:31:44 -0600

    fix: archivo del import submitform

--- app/actions/submitForm.ts
+++ app/actions/submitForm.ts
@@ -3,7 +3,7 @@
 
 'use server'
 
-import { prisma } from '@/src/lib/prisma'
+import { prisma } from '@/lib/prisma'
 
 export async function submitForm(formData: FormData) {
   const name     = formData.get('name')     as string
