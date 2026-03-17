dc9aaafb532f0068f5b196a7e235d9de00c5e5c6
Starlord427 <alanarmandomontiel@gmail.com>
2026-03-17 17:50:12 -0600

    fix: update next, y middleware

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WCF1BGaH6Mzx6PUkU1fhnKr27XwqsbOkq3U86Cb2bhg=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ export async function middleware(req: NextRequest) {
 
   const csp = [
     "default-src 'self'",
-    `script-src 'self' 'nonce-${nonce}' https://va.vercel-scripts.com https://cdn.vercel-insights.com`,
+    `script-src 'self' 'nonce-${nonce}' https://va.vercel-scripts.com https://*.vercel-insights.com https://*.vercel.app`,
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
     "img-src 'self' data: blob: https://*.supabase.co",
     "font-src 'self' https://fonts.gstatic.com",
